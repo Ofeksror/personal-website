0073036c1483e47fce77911369622cddda275978
Ofek Sror <94007441+Ofeksror@users.noreply.github.com>
2023-05-22 20:03:10 +0300

    Fix for depyloment in about.tsx

--- app/Components/About.tsx
+++ app/Components/About.tsx
@@ -9,15 +9,15 @@ const About = () => {
             <div className={styles.aboutContent}>
                 <h1>About Me</h1>
                 <p>
-                    I'm Ofek, a 17 years-old self-taught software engineer from Netayna, Israel.<br></br>
+                    Im Ofek, a 17 years-old self-taught software engineer from Netayna, Israel.<br></br>
                     I am passionate about using code to build innovative ideas.<br></br>
                     I love expanding my horizons with new technologies and learning new things.<br></br>
                 </p>
             </div>
             <div className={styles.aboutImage}>
                 <div className={styles.imageContainer}>
-                    <img src='/Ofek.jpg' alt='Ofek Sror' className={styles.personalImage} />
-                    {/* <Image src={"/Ofek.jpg"} width={1170} height={1170} alt="Ofek Sror" className={styles.personalImage} /> */}
+                    {/* <img src='/Ofek.jpg' alt='Ofek Sror' className={styles.personalImage} /> */}
+                    <Image src={"/Ofek.jpg"} width={1170} height={1170} alt="Ofek Sror" className={styles.personalImage} />
                 </div>
             </div>
         </div>
