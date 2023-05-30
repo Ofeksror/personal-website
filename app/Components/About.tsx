d71189810509dcf2fcab4936b566b1d85cfb5d0a
Ofek Sror <94007441+Ofeksror@users.noreply.github.com>
2023-05-30 16:05:41 +0300

    bug fix unescaped character

--- app/Components/About.tsx
+++ app/Components/About.tsx
@@ -9,9 +9,9 @@ const About = () => {
             <div className={styles.aboutContent}>
                 <h1>About Me</h1>
                 <p>
-                    I'm Ofek, a 17 years-old self-taught software engineer from Netanya, Israel.
-                    Completing Harvard's CS50 course ignited my love and passion for programming. <br></br>
-                    Since then, I've been on a self-guided journey, expanding my horizons with new technologies by diving into courses, guides, books, and building cool things.<br></br>
+                    I{"\'"}m Ofek, a 17 years-old self-taught software engineer from Netanya, Israel.
+                    Completing Harvard{"\'"}s CS50 course ignited my love and passion for programming. <br></br>
+                    Since then, I{"\'"}ve been on a self-guided journey, expanding my horizons with new technologies by diving into courses, guides, books, and building cool things.<br></br>
                 </p>
             </div>
             <div className={styles.aboutImage}>
